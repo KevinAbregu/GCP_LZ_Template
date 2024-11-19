3351899b0b55119623a425daae85ae667e5c5261
kevin-abregu-swo <kevin.abregu@softwareone.com>
2024-11-19 22:04:21 +0100

    feat: update dns, read from *yaml files

--- 03-networking/5.dns.auto.tfvars
+++ 03-networking/5.dns.auto.tfvars
@@ -1,36 +1,36 @@
-## Examples
-# dns = [
-#   {
-#     name        = "example-com",
-#     parent_name = "cs-net-hub"
-#     domain      = "example.com."
-#     private_visibility_config_networks = [
-#       {
-#         vpc     = "hub"
-#         project = "cs-net-hub"
-#       },
-#       {
-#         vpc              = "vpc-demolz0002-pre"
-#         complete_project = "g-prj-demolz0002-cs-net-pre"
-#       },
-#       {
-#         vpc = "projects/g-prj-demolz0002-cs-net-pro/global/networks/vpc-demolz0002-pro"
-#       },
-#     ]
-#     recordsets = [
-#       {
-#         name    = ""
-#         type    = "NS"
-#         ttl     = 300
-#         records = ["ns.example.com."]
-#       },
-#       {
-#         name    = "localhost"
-#         type    = "A",
-#         ttl     = 300,
-#         records = ["127.0.0.1"]
-#       },
-#     ]
-#   },
+# Examples
+dns = [
+  {
+    name        = "example-com",
+    parent_name = "cs-net-hub"
+    domain      = "example.com."
+    private_visibility_config_networks = [
+      {
+        vpc     = "hub"
+        project = "cs-net-hub"
+      },
+    #   {
+    #     vpc              = "vpc-demolz0002-pre"
+    #     complete_project = "g-prj-demolz0002-cs-net-pre"
+    #   },
+    #   {
+    #     vpc = "projects/g-prj-demolz0002-cs-net-pro/global/networks/vpc-demolz0002-pro"
+    #   },
+    ]
+    recordsets = [
+      {
+        name    = ""
+        type    = "NS"
+        ttl     = 300
+        records = ["ns.example.com."]
+      },
+      {
+        name    = "localhost"
+        type    = "A",
+        ttl     = 300,
+        records = ["127.0.0.1"]
+      },
+    ]
+  },
 
-# ]
+]
--- 03-networking/dns.tf
+++ 03-networking/dns.tf
@@ -4,11 +4,16 @@ locals {
       parent_complete_name               = i.parent_complete_name != null ? i.parent_complete_name : "${local.project_prefix}-${local.company_abbreviation}-${i.parent_name}"
       complete_name                      = i.complete_name != null ? i.complete_name : "${var.dns_prefix}-${local.company_abbreviation}-${i.name}"
       private_visibility_config_networks = [for k in i.private_visibility_config_networks : merge(k, { complete_project = k.project != null || k.complete_project != null ? k.project != null ? "${local.project_prefix}-${local.company_abbreviation}-${k.project}" : k.complete_project : null })]
+      
     }
   )]
   dns = [for i in local._dns : merge(i,
     {
       private_visibility_config_networks = [for k in i.private_visibility_config_networks : k.complete_project != null ? try(module.network["${k.complete_project}@${var.vpc_prefix}-${local.company_abbreviation}-${k.vpc}"].network_id, module.network["${k.complete_project}@${k.vpc}"].network_id) : k.vpc]
+      test = [for k, v in merge([
+        for f in try(fileset("dns/${i.parent_complete_name}/${i.complete_name}", "*.yaml"), []) :
+        yamldecode(file("dns/${i.parent_complete_name}/${i.complete_name}/${f}"))
+      ]...): v]
     }
   )]
 }
@@ -18,9 +23,8 @@ locals {
 
 module "dns" {
   source   = "terraform-google-modules/cloud-dns/google"
-  version  = "5.2.0"
+  version  = "5.3.0"
   for_each = { for i in local.dns : "${i.parent_complete_name}@${i.complete_name}" => i }
-
   name                               = each.value.complete_name
   project_id                         = each.value.parent_complete_name
   domain                             = each.value.domain
@@ -35,6 +39,6 @@ module "dns" {
   default_key_specs_zone             = each.value.default_key_specs_zone
   force_destroy                      = each.value.force_destroy
   service_namespace_url              = each.value.service_namespace_url
-  recordsets                         = each.value.recordsets
+  recordsets                         = concat(each.value.recordsets, tolist(each.value.test))
   enable_logging                     = each.value.enable_logging
 }
