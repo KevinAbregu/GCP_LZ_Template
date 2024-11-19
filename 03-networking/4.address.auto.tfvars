a3232175ded0a61d7e8e597ccf43a7a1302c3135
kevin-abregu-swo <kevin.abregu@softwareone.com>
2024-11-19 22:37:58 +0100

    feat: update nat module

--- 03-networking/4.address.auto.tfvars
+++ 03-networking/4.address.auto.tfvars
@@ -1,19 +1,19 @@
 ## Examples
 address = [
-  #   {
-  #     parent_name  = "cs-net-hub"
-  #     address_type = "EXTERNAL"
-  #     names        = ["ip-nat-01"]
-  #     global       = false
-  #     addresses    = [""]
+    {
+      parent_name  = "cs-net-hub"
+      address_type = "EXTERNAL"
+      names        = ["ip-nat-01"]
+      global       = false
+      addresses    = [""]
 
-  #   },
-  #   # Use Examples
-  #   {
-  #       parent_name = "cs-net-pro"
-  #       subnet_name = "pro-principal"
-  #       names = ["test01","test02", "cinco"]
-  #       addresses = ["","", "10.0.2.5"]
-  #   },
+    },
+    # Use Examples
+    {
+        parent_name = "cs-net-pro"
+        subnet_name = "pro-principal"
+        names = ["test01","test02", "cinco"]
+        addresses = ["","", "10.0.2.5"]
+    },
 
 ]
--- 03-networking/nat.tf
+++ 03-networking/nat.tf
@@ -10,26 +10,26 @@ locals {
 }
 module "cloud-nat" {
   source                              = "terraform-google-modules/cloud-nat/google"
-  version                             = "~> 5.0"
-  count                               = length(local._nats)
-  project_id                          = local._nats[count.index].parent_complete_name
-  name                                = local._nats[count.index].complete_name
-  region                              = local._nats[count.index].region
-  icmp_idle_timeout_sec               = local._nats[count.index].icmp_idle_timeout_sec
-  min_ports_per_vm                    = local._nats[count.index].min_ports_per_vm
-  max_ports_per_vm                    = local._nats[count.index].max_ports_per_vm
-  nat_ips                             = local._nats[count.index].nat_ips
-  router                              = local._nats[count.index].router_complete_name
-  source_subnetwork_ip_ranges_to_nat  = local._nats[count.index].source_subnetwork_ip_ranges_to_nat
-  tcp_established_idle_timeout_sec    = local._nats[count.index].tcp_established_idle_timeout_sec
-  tcp_transitory_idle_timeout_sec     = local._nats[count.index].tcp_transitory_idle_timeout_sec
-  tcp_time_wait_timeout_sec           = local._nats[count.index].tcp_time_wait_timeout_sec
-  udp_idle_timeout_sec                = local._nats[count.index].udp_idle_timeout_sec
-  subnetworks                         = local._nats[count.index].subnetworks
-  log_config_enable                   = local._nats[count.index].log_config_enable
-  log_config_filter                   = local._nats[count.index].log_config_filter
-  enable_dynamic_port_allocation      = local._nats[count.index].enable_dynamic_port_allocation
-  enable_endpoint_independent_mapping = local._nats[count.index].enable_endpoint_independent_mapping
+  version                             = "~> 5.3"
+  for_each                            = {for i in local._nats: "${i.parent_complete_name}@${i.complete_name}" => i}
+  project_id                          = each.value.parent_complete_name
+  name                                = each.value.complete_name
+  region                              = each.value.region
+  icmp_idle_timeout_sec               = each.value.icmp_idle_timeout_sec
+  min_ports_per_vm                    = each.value.min_ports_per_vm
+  max_ports_per_vm                    = each.value.max_ports_per_vm
+  nat_ips                             = each.value.nat_ips
+  router                              = each.value.router_complete_name
+  source_subnetwork_ip_ranges_to_nat  = each.value.source_subnetwork_ip_ranges_to_nat
+  tcp_established_idle_timeout_sec    = each.value.tcp_established_idle_timeout_sec
+  tcp_transitory_idle_timeout_sec     = each.value.tcp_transitory_idle_timeout_sec
+  tcp_time_wait_timeout_sec           = each.value.tcp_time_wait_timeout_sec
+  udp_idle_timeout_sec                = each.value.udp_idle_timeout_sec
+  subnetworks                         = each.value.subnetworks
+  log_config_enable                   = each.value.log_config_enable
+  log_config_filter                   = each.value.log_config_filter
+  enable_dynamic_port_allocation      = each.value.enable_dynamic_port_allocation
+  enable_endpoint_independent_mapping = each.value.enable_endpoint_independent_mapping
   depends_on = [
     module.cloud_router,
     module.address
